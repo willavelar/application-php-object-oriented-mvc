2b221cc5adb7a43dfb9006fb3521459b7da60f71
William Avelar <avelardesigner@gmail.com>
2023-07-18 14:37:55 -0300

    Put node modules in gitignore and rename containers

--- backend/app/controllers/Pages.php
+++ backend/app/controllers/Pages.php
@@ -5,21 +5,20 @@ namespace App\Controllers;
 use App\Libraries\Controller;
 use App\Libraries\Model;
 
-class Exemple extends Controller
+class Pages extends Controller
 {
-    private Model $model;
 
     public function __construct()
     {
-        $this->model = $this->model('Exemple');
+
     }
 
     public function index()
     {
         $data = [
-            'title' => 'Welcome'
+            'title' => 'SharePosts'
         ];
 
-        $this->view('exemple/index', $data);
+        $this->view('pages/index', $data);
     }
 }
--- backend/app/views/index.php
+++ backend/app/views/index.php
@@ -1,4 +1,3 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
 <h1><?= $data['title'] ?></h1>
-<p>This is the PHP framework</p>
 <?php require APPROOT.'/views/inc/footer.php'; ?>
